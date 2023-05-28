fe5b773a53b3da69061aa8e375b8beee8e0be611
Parkash <parkashsarangani@hotmail.com>
2023-05-28 18:06:45 +0200

    Added Jacoco plugin

--- DocumentService/src/test/java/com/qbd/dms/documentservice/repos/DocumentRepoTest.java
+++ DocumentService/src/test/java/com/qbd/dms/documentservice/repos/DocumentRepoTest.java
@@ -13,16 +13,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
--- DocumentService/src/test/java/com/qbd/dms/documentservice/services/DocumentServiceTest.java
+++ DocumentService/src/test/java/com/qbd/dms/documentservice/services/DocumentServiceTest.java
@@ -2,18 +2,18 @@ package com.qbd.dms.documentservice.services;
 
 
 
+import com.qbd.dms.documentservice.utility.PdfUtility;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(DocumentService.class)
 public class DocumentServiceTest {
+
+    @MockBean
+    PdfUtility pdfUtility;
 
     @Test
     public void testSaveFile(){
-
+        Assertions.assertTrue(true);
     }
 }
