330dce63f7586b45cbee8bc5407855bac8a4360e
SYSTEMS\om.parkash <om.parkash@systemsltd.com>
2023-05-29 11:40:41 +0500

    Some test cases

--- DocumentService/src/test/java/com/qbd/dms/documentservice/services/DocumentServiceTest.java
+++ DocumentService/src/test/java/com/qbd/dms/documentservice/services/DocumentServiceTest.java
@@ -1,19 +1,33 @@
 package com.qbd.dms.documentservice.services;
 
 
-
 import com.qbd.dms.documentservice.utility.PdfUtility;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.print.Doc;
+import java.io.IOException;
 
 public class DocumentServiceTest {
 
     @MockBean
     PdfUtility pdfUtility;
 
+
     @Test
-    public void testSaveFile(){
+    @DisplayName("always true")
+    public void test() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("NullPointerException case")
+    public void testSaveFile() throws IOException {
+        DocumentService documentService = new DocumentService();
+        Assertions.assertThrowsExactly(NullPointerException.class,() -> documentService.saveFile(null));
     }
 }
